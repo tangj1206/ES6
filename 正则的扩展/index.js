782769342c072418346677ed16ca144bfe7867bf
tangj1206 <1798516284@qq.com>
2018-12-12 17:18:32 +0800

    正则的扩展

---
+++ 正则的扩展/index.js
@@ -0,0 +1,21 @@
+//ES5
+/* //第一种参数字符串
+var regex = new RegExp('xyz', 'i');
+//等价于
+var regex = /xyz/i;
+
+//第二种正则表达式
+var regex = new RegExp(/xyz/i);
+//等价于
+var regex = /xyz/i; */
+
+/* var s = 'aaa_aa_a';
+var r1 = /a/g;
+var r2 = /a+/y;
+
+console.log(r1.exec(s));
+console.log(r2.exec(s));
+
+ */
+
+ 
